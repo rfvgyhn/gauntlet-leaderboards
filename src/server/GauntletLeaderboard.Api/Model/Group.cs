65b6b59bf493b5f76ef2fb749953cc45070beb7b
Chris <bahrango@hotmail.com>
2015-04-08 19:27:34 -0600

    add id to group

--- src/server/GauntletLeaderboard.Api/Model/Group.cs
+++ src/server/GauntletLeaderboard.Api/Model/Group.cs
@@ -4,6 +4,7 @@ namespace GauntletLeaderboard.Api.Model
 {
     public class Group
     {
+        public string Id { get { return Name.ToLower(); } }
         public string Name { get; set; }
         public bool IsActive { get; set; }
         public IEnumerable<Group> SubGroups { get; set; }
