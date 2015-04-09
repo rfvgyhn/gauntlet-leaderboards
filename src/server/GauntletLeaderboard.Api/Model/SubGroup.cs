27dc5d47bff52715c990642fb900a02b844c1980
Chris <bahrango@hotmail.com>
2015-04-08 19:28:02 -0600

    add id, group and child leaderboards to sub group

--- src/server/GauntletLeaderboard.Api/Model/SubGroup.cs
+++ src/server/GauntletLeaderboard.Api/Model/SubGroup.cs
@@ -7,7 +7,10 @@ namespace GauntletLeaderboard.Api.Model
 {
     public class SubGroup
     {
+        public string Id { get { return Name.ToLower(); } }
+        public string Group { get; set; }
         public string Name { get; set; }
         public bool IsActive { get; set; }
+        public IEnumerable<Leaderboard> Leaderboards { get; set; }
     }
 }
