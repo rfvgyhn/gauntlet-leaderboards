c86fe92f9ba84bb0b222f82a41c680ecde2cbbc2
Chris <bahrango@hotmail.com>
2015-04-11 15:34:04 -0600

    highlight current nav on leaderboards section

---
+++ GauntletLeaderboard.Web/Scripts/main.js
@@ -0,0 +1,13 @@
+﻿(function ($) {
+    $(function () {
+        $(".container[role=main] .nav a").each(function () {
+            var $this = $(this);
+            $this.removeClass("active");
+            var path = location.href.substr(0, this.href.length).toLowerCase();
+            var match = this.href.toLowerCase() == path;
+
+            if (match)
+                $this.add($this.parent("li")).addClass("active");
+        })
+    });
+})(jQuery);
