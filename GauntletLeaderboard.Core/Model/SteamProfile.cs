565e9d9fd730f8dd724749fc6256a6917629083b
Chris <bahrango@hotmail.com>
2015-04-11 21:26:38 -0600

    use vanity name on leaderboard profile links

--- GauntletLeaderboard.Core/Model/SteamProfile.cs
+++ GauntletLeaderboard.Core/Model/SteamProfile.cs
@@ -17,11 +17,23 @@ namespace GauntletLeaderboard.Core.Model
         public long SteamId { get; set; }
         public string PersonaName { get; set; }
         public string ProfileUrl { get; set; }
+        
         public string Avatar { get; set; }
         public string AvatarMedium { get; set; }
         public string AvatarFull { get; set; }
         public PersonaState PersonaState { get; set; }
         public float? AchievementPercentage { get; set; }
         public IEnumerable<SteamBadge> Badges { get; set; }
+
+        public string VanityName
+        {
+            get
+            {
+                if (ProfileUrl.Contains("/id"))
+                    return ProfileUrl.Substring(ProfileUrl.LastIndexOf("/id") + 4).TrimEnd('/');
+
+                return null;
+            }
+        }
     }
 }
