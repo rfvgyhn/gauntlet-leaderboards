006d0c6da27c6c685be102ca1b58d5b2e29c96cb
Chris <bahrango@hotmail.com>
2015-04-15 20:32:53 -0600

    add toons and difficulty to leaderboards

--- GauntletLeaderboard.Core/Model/Entry.cs
+++ GauntletLeaderboard.Core/Model/Entry.cs
@@ -1,19 +1,78 @@
-﻿using System.Xml.Serialization;
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
 
 namespace GauntletLeaderboard.Core.Model
 {
     [XmlRoot("entry")]
     public class Entry
     {
+        const int DifficultyOffset = 1;
+        const int CharacterOffset = 4;
+        string detailsString;
+        List<Toon> toons;
+
         [XmlElement("steamid")]  
         public long SteamId { get; set; }
-        [XmlIgnore]
-        public SteamProfile Player { get; set; }
+
+        [XmlElement("details")]
+        public string DetailsString {
+            get
+            {
+                return detailsString;
+            }
+            set
+            {
+                detailsString = value;
+
+                ParseAndSetDetails(value);
+            }
+        }
+
         [XmlElement("score")]
         public int Score { get; set; }
+
         [XmlElement("rank")]
         public int Rank { get; set; }
+
         [XmlIgnore]
         public Leaderboard Leaderboard { get; set; }
+
+        [XmlIgnore]
+        public byte[] Details { get; private set; }
+
+        [XmlIgnore]
+        public SteamProfile Player { get; set; }
+
+        [XmlIgnore]
+        public IEnumerable<Toon> Toons { get { return toons; } }
+
+        [XmlIgnore]
+        public Difficulty Difficulty { get; private set; }
+
+        private void ParseAndSetDetails(string hexString)
+        {
+            Details = StringToByteArray(hexString);
+
+            toons = new List<Toon>();
+            var toon = (Toon)Details[CharacterOffset];
+
+            foreach (Toon value in Enum.GetValues(toon.GetType()))
+            {
+                if (toon.HasFlag(value))
+                    toons.Add(value);
+            }
+            Difficulty = (Difficulty)Details[DifficultyOffset];
+        }
+
+        // http://stackoverflow.com/a/311179/182821
+        private static byte[] StringToByteArray(string hex)
+        {
+            int NumberChars = hex.Length;
+            byte[] bytes = new byte[NumberChars / 2];
+            for (int i = 0; i < NumberChars; i += 2)
+                bytes[i / 2] = Convert.ToByte(hex.Substring(i, 2), 16);
+            return bytes;
+        }
     }
 }
---
+++ GauntletLeaderboard.Core/Model/Toon.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GauntletLeaderboard.Core.Model
+{
+    [Flags]
+    public enum Toon
+    {
+        Warrior = 1,
+        Valkryie = 2,
+        Wizard = 4,
+        Elf = 8,
+        Necromancer = 16
+    }
+}
